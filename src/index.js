546d705c4ec5c6dcfece8f488f9d892071e70f74
Nastishka <anastasiya.matronkina.com>
2020-03-22 22:29:40 +0300

    feat: implement function that identifies if all brackets are written in correct order

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsMap = bracketsConfig.reduce(
+        (map, currentItem) => {
+            map[currentItem[1]] = currentItem[0];
+            return map;
+        }
+        , {});
+
+    let brackets = str.split('');
+    let openBrackets = [];
+    while (brackets.length > 0) {
+        let bracket = brackets.shift();
+        let isOpenBracket = Object.values(bracketsMap).indexOf(bracket) >= 0;
+        let isCloseBracket = Object.keys(bracketsMap).indexOf(bracket) >= 0;
+        if (isOpenBracket && isCloseBracket) {
+            isOpenBracket = openBrackets.indexOf(bracket) < 0;
+        }
+        if (isOpenBracket) {
+            openBrackets.push(bracket);
+        } else {
+            if (isCloseBracket) {
+                let lastOpenBracket = openBrackets.pop();
+                if (lastOpenBracket !== bracketsMap[bracket]) {
+                    return false;
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    return brackets.length === 0 && openBrackets.length === 0;
 }
